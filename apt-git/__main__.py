a763196f6066c53b1f85cab753a48fbe404b1271
Steffan Sluis <steffansluis@gmail.com>
2014-04-10 02:35:11 +0200

    Random changes

--- apt-git/__main__.py
+++ apt-git/__main__.py
@@ -14,8 +14,6 @@ VERBOSE = False
 MAIN_DIR = os.environ['HOME'] + '/apt-git/'
 
 def install(packages):
-	print "Installing"
-
 	installers = list()
 
 	for package in packages:
@@ -27,7 +25,6 @@ def install(packages):
 		installer.join()
 
 def remove(packages):
-	print "remove"
 	removers = list()
 
 	for package in packages:
@@ -37,97 +34,28 @@ def remove(packages):
 	# Wait untill all removers are done
 	for remover in removers:
 		remover.join()
-	
-# Global functions
 
-# def weave(**args):
-# 	thread = threading.Thread(**args)
-# 	thread.daemon = True
-# 	thread.start()
-# 	return thread
+def update(packages):
+	updaters = list()
 
-# def find(repository):
-# 	for key, value in repo_dict.iteritems():   # iter on both keys and values
-# 		if key.startswith(repository):
-# 			res[key]=value
+	for package in packages:
+		updater = package.update_async()
+		updaters.append(updater)
 
-# 		return res
+	# Wait untill all removers are done
+	for updater in updaters:
+		updater.join()
 
+def upgrade(packages):
+	upgraders = list()
 
-# # Multiprocessable functions
+	for package in packages:
+		upgrader = package.upgrade_async()
+		upgraders.append(upgrader)
 
-# # Check if the repository at source exists
-# def check_repository_address(source, repository, valid_repository_queue):
-# 	# Check if https connection can be made
-# 	addr = "https://"+source+"/"+repository+".git"
-# 	if VERBOSE:
-# 		print "Now checking for the existance of",addr+"."
-# 	http = httplib.HTTPSConnection(source, 443)
-# 	http.request("HEAD", "/"+repository)
-# 	response = http.getresponse()
-
-# 	if VERBOSE:
-# 		print "Connecting to https://"+source+"/"+repository,", reponse status:",str(response.status)+"."
-	
-# 	if response.status==200:
-# 		print "Valid repository found: ",addr
-# 		valid_repository_queue.put(addr)
-
-# def pull_repository_data(addr, branch, repository_data_queue):
-# 		print "Checking latest commit date for",addr,"on branch '",branch+"'."
-# 		repo = Repo.clone_from(addr, './.apt-git/',commit='refs/heads/master')
-# 		latest = repo.heads.master.commit.authored_date
-
-# 		date = datetime.strftime(datetime.fromtimestamp(latest),'%Y-%m-%d')
-# 		time = datetime.strftime(datetime.fromtimestamp(latest),'%H:%M:%S')
-# 		if VERBOSE:
-# 			print "Lastest commit was on",date,"at",time+"."
-# 		repository_data_queue.put((latest*-1,(addr,branch))) # Smallest entry is the latest updated, so it's on top of the queue
-
-
-# # Multiprocessing helpers
-
-# def valid_repository_queue_watcher(valid_repository_queue, repository_data_queue):
-# 	while(True):
-# 		addr = valid_repository_queue.get(True)
-# 		branch = 'master'
-# 		weave(target=pull_repository_data, args=(addr, branch, repository_data_queue))
-
-# Command functionalities
-
-# Install new reposities
-
-
-	# valid_repository_queue = Queue.Queue()
-	# repository_data_queue = Queue.PriorityQueue()
-
-	# weave(target=valid_repository_queue_watcher, args=(valid_repository_queue, repository_data_queue))
-
-	# for repository in repositories:
-	# 		for source in sources:
-	# 			checker = weave(target=check_repository_address, args=(source, repository, valid_repository_queue))
-
-
- #  # Wait while the last checker thread is still alive
-	# while(checker.isAlive()):
-	# 	pass
-
-	# try:
-	# 	repository = repository_data_queue.get(True,8) # Wait for next repository for 8 seconds, else time out
-	# except Queue.Empty:
-	# 	print 'No valid repositories were found. (did you spell everything right?)'
-	# 	exit(0)
-
-	# # Pick the repository with the newest commit on the branch
-	# (latest,(addr,branch)) = repository
-	# print "The most up to date repository that was found is",addr+"."
-	# print "Installing the repository at ",addr,"to '",MAIN_DIR+"'."
-
-	# if not(os.path.exists(MAIN_DIR)):
-	# 	os.makedirs(MAIN_DIR,0755)
-	# repository = Repo.clone_from(addr, MAIN_DIR)
-	# repository.remotes.origin.pull(branch)
-
+	# Wait untill all removers are done
+	for upgrader in upgraders:
+		upgrader.join()
 
 # Define and parse arguments
 
--- apt-git/package.py
+++ apt-git/package.py
@@ -18,27 +18,27 @@ class Package:
 		self.install_dir = os.path.join(main_dir, self.name)
 		self.branch = branch
 		self.updated = Queue.PriorityQueue()
-		
-
 
 		self.url = "https://"+os.path.join(self.source,self.name)
 
-		self.installed = os.path.exists(self.install_dir)
-		print self.installed
+		self.installed = os.path.exists(os.path.join(self.install_dir,'.git'))
+
+		if self.installed:
+			self.repository = Repo(self.install_dir)
 
 	def install(self):
 		# Create temp dir
 		if not self.installed:
 			os.makedirs(self.install_dir, 0775)
 
-		print "The most up to date repository that was found is ", self.url, "."
 		print "Installing the repository at ", self.url, "to ", self.install_dir, "."
 
 		self.repository = Repo.init(self.install_dir)
-		self.repository.create_remote(self.source, self.url)
+		if not self.repository.remotes:
+			self.repository.create_remote(self.source, self.url)
 		
 		# Get the most recent data
-		#self.update()
+		self.update()
 		#self.upgrade()
 
 	def install_async(self):
@@ -48,33 +48,48 @@ class Package:
 
 
 	def update(self):
-		print "Checking latest commit date for",self.url+"."
-		remote = self.repository.refs[self.branch]
-		remote.fetch()
-		latest_commit_at = self.repository[self.branch].commit.authord_date
+		print "Checking active branch."
+		self.branch = self.repository.active_branch.name
 
+		print "Fetching latest data from remotes"
+		for remote in self.repository.remotes:
+			print "Checking latest commit date for remote",remote.name,"on branch",self.branch+"."
 
-		date = datetime.strftime(datetime.fromtimestamp(latest),'%Y-%m-%d')
-		time = datetime.strftime(datetime.fromtimestamp(latest),'%H:%M:%S')
-		if VERBOSE:
-			print "Lastest commit was on",date,"at",time+"."
-		self.updated.put((latest*-1,(addr,branch))) # Smallest entry is the latest updated, so it's on top of the queue
+			# Check if remote has the requested branch
+			try:
+				remote.fetch("refs/heads/"+self.branch+":refs/remotes/"+remote.name+"/"+self.branch)
+			except:
+				print "The branch does not exist on remote",remote.name+"."
+				continue
+
+			latest_commit_at = remote.refs[self.branch].commit.authored_date
+
+			date = datetime.strftime(datetime.fromtimestamp(latest_commit_at),'%Y-%m-%d')
+			time = datetime.strftime(datetime.fromtimestamp(latest_commit_at),'%H:%M:%S')
+			print "Lastest commit for remote",remote.name," was on",date,"at",time+"."
+			self.updated.put((latest_commit_at*-1,remote)) # Smallest entry is the latest updated, so it's on top of the queue
+	
+	def update_async(self):
+		updater = threading.Thread(target=self.update)
+		updater.start()
+		return updater
 
 	def upgrade(self):
-		self.repository.remotes.origin.pull(self.branch)
-		
+		return
+		#remote.pull(self.branch)
+
+	def upgrade_async(self):
+		upgrader = threading.Thread(target=self.upgrade)
+		upgrader.start()
+		return upgrader
+
 	def remove(self):
 		if not self.installed: 
-			print "Target directory not found."
+			print "Repository",self.name,"is not installed."
 			return
 		shutil.rmtree(os.path.dirname(self.install_dir))
-
 
 	def remove_async(self):
 		remover = threading.Thread(target=self.remove)
 		remover.start()
 		return remover
-
-
-	def addSource(self, name, url):
-		pass
